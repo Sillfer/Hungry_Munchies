8a01af286f5fc005886f07bd369b3cd3ad3e0339
Sillfer <gliouinemahdi0@gmail.com>
2023-02-18 21:40:50 +0100

    First Step

---
+++ Hungry_Munchies/src/main/java/com/hungry_munchies/Hungry_Munchies/HungryMunchiesApplication.java
@@ -0,0 +1,13 @@
+package com.hungry_munchies.Hungry_Munchies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HungryMunchiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HungryMunchiesApplication.class, args);
+	}
+
+}
---
+++ Hungry_Munchies/src/test/java/com/hungry_munchies/Hungry_Munchies/HungryMunchiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hungry_munchies.Hungry_Munchies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HungryMunchiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
